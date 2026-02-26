c8439d265d31fe1459cf9f53d934a5dc099bef18
Attila Tóth <attila.toth13@tesco.com>
2026-02-26 15:02:32 +0100

    initial commit

---
+++ src/test/java/hu/elte/fullstack/fullstack_programming_backend/FullstackProgrammingBackendApplicationTests.java
@@ -0,0 +1,13 @@
+package hu.elte.fullstack.fullstack_programming_backend;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class FullstackProgrammingBackendApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}

ae68513b1d60ec0bcca054e4e5297269720c32ee
Attila Tóth <attila.toth13@tesco.com>
2026-03-02 11:03:16 +0100

    2026-03-02 - add OpenAPI

--- src/main/java/hu/elte/fullstack/fullstack_programming_backend/ExampleController.java
+++ src/main/java/hu/elte/fullstack/fullstack_programming_backend/ExampleController.java
@@ -3,6 +3,8 @@ package hu.elte.fullstack.fullstack_programming_backend;
 import static org.springframework.http.ResponseEntity.noContent;
 import static org.springframework.http.ResponseEntity.ok;
 
+import io.swagger.v3.oas.annotations.media.Content;
+import io.swagger.v3.oas.annotations.responses.ApiResponse;
 import org.springframework.http.ResponseEntity;
 import org.springframework.security.core.context.SecurityContextHolder;
 import org.springframework.web.bind.annotation.GetMapping;
@@ -13,22 +15,23 @@ import org.springframework.web.bind.annotation.RestController;
 public class ExampleController {
 
     @GetMapping("/public")
+    @ApiResponse(responseCode = "200", content = @Content(mediaType = "application/json"))
     public ResponseEntity<?> publicEndpoint() {
-        return ok("Hello, public!");
+        return ok("{\"message\": \"Hello, anonymous!\"}");
     }
 
     @PostMapping("/login")
+    @ApiResponse(responseCode = "204")
+    @ApiResponse(responseCode = "401")
     public ResponseEntity<?> loginEndpoint() {
         return noContent().build();
     }
 
     @GetMapping("/protected")
+    @ApiResponse(responseCode = "200", content = @Content(mediaType = "application/json"))
+    @ApiResponse(responseCode = "401")
     public ResponseEntity<?> protectedEndpoint() {
-
         String name = SecurityContextHolder.getContext().getAuthentication().getName();
-
-        return ok("Hello, " + name);
+        return ok("{\"message\": \"Hello, " + name + "!\"}");
     }
-
-
 }
